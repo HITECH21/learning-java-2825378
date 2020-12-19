9edd268a74729a5654f2b45bb202a7d3b1cdbc16
Lessley Dennington <oengdpswe@microsoft.com>
2020-12-18 19:44:42 -0800

    Sting concatenation

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int studentAge = 15;
         double studentGPA = 3.45;
         String studentFirstName = "Kayla";
         String studentLastName =  "Hammond";
-        char studentFirstInitial = studentFirstName.charAt(0);
-        char studentLastInitial = studentLastName.charAt(0);
 
-        System.out.println(studentAge);
-        System.out.println(studentGPA);
-        System.out.println(studentFirstInitial);
-        System.out.println(studentLastInitial);
-        System.out.println(studentFirstName);
-        System.out.println(studentLastName);
+        System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA);
     }
 }
