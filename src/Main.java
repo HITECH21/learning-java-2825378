0a80b46807cdd630943cfdf20f7a5f1d7c3f5968
Lessley Dennington <oengdpswe@microsoft.com>
2020-12-18 19:48:41 -0800

    Get user input

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         double studentGPA = 3.45;
         String studentFirstName = "Kayla";
         String studentLastName =  "Hammond";
 
-        System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA);
+        System.out.println(studentFirstName + " " +
+                studentLastName + " has a GPA of " + studentGPA);
+        System.out.println("What do you want to update it to?");
+
+        Scanner input = new Scanner(System.in);
+        studentGPA = input.nextDouble();
+        System.out.println(studentFirstName + " " + studentFirstName +
+                " has a GPA of " + studentGPA);
     }
 }
